6808d9a1617f538255f4ace714cdb2a1c16ac056
keskumar5 <keshavkumar.er97@gmail.com>
2025-04-21 15:56:27 +0530

    updated prop

---
+++ src/main/java/com/example/kafka_streams_processor/configuration/KafkaStreamsAppConfig.java
@@ -0,0 +1,9 @@
+package com.example.kafka_streams_processor.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@Configuration
+@EnableKafkaStreams
+public class KafkaStreamsAppConfig {
+}
