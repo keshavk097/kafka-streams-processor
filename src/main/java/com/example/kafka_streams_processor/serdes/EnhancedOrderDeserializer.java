bf22b0f5a90e47696a54d948ee5e669f84153ea4
keskumar5 <keshavkumar.er97@gmail.com>
2025-04-21 22:57:25 +0530

    updated prop

--- src/main/java/com/example/kafka_streams_processor/serdes/EnhancedOrderDeserializer.java
+++ src/main/java/com/example/kafka_streams_processor/serdes/EnhancedOrderDeserializer.java
@@ -2,13 +2,21 @@ package com.example.kafka_streams_processor.serdes;
 
 import com.example.kafka_streams_processor.model.ProcessedStandingOrder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.kafka.common.serialization.Deserializer;
 
 public class EnhancedOrderDeserializer implements Deserializer<ProcessedStandingOrder> {
+    private final ObjectMapper objectMapper;
+
+    public EnhancedOrderDeserializer() {
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.registerModule(new JavaTimeModule()); // <-- Fix here
+    }
+
     @Override
     public ProcessedStandingOrder deserialize(String topic, byte[] data) {
         try {
-            return new ObjectMapper().readValue(data, ProcessedStandingOrder.class);
+            return objectMapper.readValue(data, ProcessedStandingOrder.class);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/example/kafka_streams_processor/serdes/OrderDeserializer.java
+++ src/main/java/com/example/kafka_streams_processor/serdes/OrderDeserializer.java
@@ -2,13 +2,21 @@ package com.example.kafka_streams_processor.serdes;
 
 import com.example.kafka_streams_processor.model.StandingOrder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.kafka.common.serialization.Deserializer;
 
 public class OrderDeserializer implements Deserializer<StandingOrder> {
+    private final ObjectMapper objectMapper;
+
+    public OrderDeserializer() {
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.registerModule(new JavaTimeModule()); // <-- Fix here
+    }
+
     @Override
     public StandingOrder deserialize(String topic, byte[] data) {
         try {
-            return new ObjectMapper().readValue(data, StandingOrder.class);
+            return objectMapper.readValue(data, StandingOrder.class);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
