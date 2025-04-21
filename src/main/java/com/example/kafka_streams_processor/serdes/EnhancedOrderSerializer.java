e98399269f40b1d81e21f78b541de86d5d152639
keskumar5 <keshavkumar.er97@gmail.com>
2025-04-21 23:07:44 +0530

    updated prop

--- src/main/java/com/example/kafka_streams_processor/serdes/EnhancedOrderSerializer.java
+++ src/main/java/com/example/kafka_streams_processor/serdes/EnhancedOrderSerializer.java
@@ -2,13 +2,23 @@ package com.example.kafka_streams_processor.serdes;
 
 import com.example.kafka_streams_processor.model.ProcessedStandingOrder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.kafka.common.serialization.Serializer;
 
 public class EnhancedOrderSerializer implements Serializer<ProcessedStandingOrder> {
+    private final ObjectMapper objectMapper;
+
+    public EnhancedOrderSerializer() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
     @Override
     public byte[] serialize(String topic, ProcessedStandingOrder data) {
         try {
-            return new ObjectMapper().writeValueAsBytes(data);
+            return objectMapper.writeValueAsBytes(data);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
